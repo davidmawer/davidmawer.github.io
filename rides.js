02eb13fe967a199b0291956410fc9c2a44021101
dmawer <dmawer@buffalo.edu>
2018-11-27 10:27:23 -0500

    All Activities 9/8/18

--- rides.js
+++
@@ -1,5 +0,0 @@
-        var runLayer = omnivore.gpx('Rides/1.gpx', null, customLayerRide)
-            .on('ready', function() {
-                map.fitBounds(runLayer.getBounds());
-            })
-            .addTo(map);
