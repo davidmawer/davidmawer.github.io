d90dca4127013abeb76a5ceb1f69e3900785ff4a
dmawer <dmawer@buffalo.edu>
2019-02-08 10:02:13 -0500

    Update sketch.js

--- p5/Strava/sketch.js
+++ p5/Strava/sketch.js
@@ -5,24 +5,14 @@ var accessTokenDave = "7199a0f11d1a60bcef9005bf7291ebcde9227ee4";
 
 // Configure OAuth2 access token for authorization: strava_oauth
 var strava_oauth = defaultClient.authentications['strava_oauth'];
-strava_oauth.accessToken = accessTokenDave;
+strava_oauth.accessToken = "7199a0f11d1a60bcef9005bf7291ebcde9227ee4";
 
 var api = new StravaApiV3.ActivitiesApi()
 
-var name = name_example; // {String} The name of the activity.
-
-var type = type_example; // {String} Type of activity. For example - Run, Ride etc.
-
-var startDateLocal = startDateLocal_example; // {String} ISO 8601 formatted date time.
-
-var elapsedTime = 56; // {Integer} In seconds.
+var id = 2122222352; // {Long} The identifier of the activity.
 
 var opts = { 
-  'description': description_example, // {String} Description of the activity.
-  'distance': 3.4, // {Float} In meters.
-  'trainer': 56, // {Integer} Set to 1 to mark as a trainer activity.
-  'photoIds': photoIds_example, // {String} List of native photo ids to attach to the activity.
-  'commute': 56 // {Integer} Set to 1 to mark as commute.
+  'includeAllEfforts': true // {Boolean} To include all segments efforts.
 };
 
 var callback = function(error, data, response) {
@@ -32,7 +22,7 @@ var callback = function(error, data, response) {
     console.log('API called successfully. Returned data: ' + data);
   }
 };
-api.createActivity(name, type, startDateLocal, elapsedTime, opts, callback);
+api.getActivityById(id, opts, callback);
 
 function setup() {
 background(red);
